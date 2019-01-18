12040912a649024a5a7a001b3d4e8ad63b82e978
Brydon Gibson <brygibson@blackberry.com>
2019-01-18 18:31:32 -0500

    will it work? maybe, have I tested it? hell no

--- nightlight.ino
+++ nightlight.ino
@@ -3,14 +3,15 @@
 
 #define LEDPIN 3
 
-unsigned long secondsOfDay = 24000;
+unsigned long secondsOfDay = 21600;
+;
 unsigned long minutesOfDay = 0;
 unsigned int hoursOfDay = 0;
 int ledBrightness = 0;
 
 
 SIGNAL(TIMER1_COMPA_vect){
-  secondsOfDay++; //count one second
+  secondsOfDay = secondsOfDay >= 86400 ? 0 : secondsOfDay + 1;
   hoursOfDay = secondsOfDay/(60 * 60);
   minutesOfDay = (secondsOfDay - (hoursOfDay * 60 * 60))/60;
 }
@@ -41,9 +42,15 @@ void setup() {
 void loop() {
   delay(2000); //just sleep for a long time. Timer interrupts are managing everything
   if (secondsOfDay >= (86400)) secondsOfDay = 0;
-  if (((hoursOfDay >= 6 && minutesOfDay > 30) || hoursOfDay > 7) && hoursOfDay < 20) { //it's the morning, we should be incrementing
-    if (ledBrightness < 255) ledBrightness++;
+  if (hoursOfDay == 6) { //it's the morning, we should be incrementing
+    ledBrightness < 255 ? ledBrightness++: 0;
     //Serial.println("It's morning");
+  }
+  if (hoursOfDay == 7){
+    ledBrightness = 0;
+  }
+  if (hoursOfDay == 21 && minutesOfDay == 59){
+    ledBrightness = 255;
   }
   if (hoursOfDay >= 22){
     ledBrightness > 0 ? ledBrightness-- : 0; //decrememnt if we need to
